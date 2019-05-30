902b9251407bb82ea8b08666458e1615f2e73543
stephenliu7 <35360739+kcliu7@users.noreply.github.com>
2019-05-29 22:08:56 -0700

    Update second_classifier.py to unzip corpus

---
+++ client.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import rpyc
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 8001        # Port to listen on (non-privileged ports are > 1023)
+
+def predict():
+    text = "It's not good enough. "
+    text_news = "Hugh Grant Marries For The First Time At Age 57"
+    try:
+        with rpyc.connect(HOST, PORT) as conn:
+            t = conn.root.predict(text)
+            print(t)
+            t = conn.root.predict_news(text_news)
+            print(t)
+
+    except Exception as e:
+        raise e
+
+
+predict()
